ae950fefdf07c5a736610cf9c0df57424a9f81d8
takeshix <takeshix@adversec.com>
2022-05-08 13:05:57 +0200

    Fixed var name

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from ssllabs import vars
 
 setup(name='python-ssllabs',
-    version=vars.__version__,
+    version=vars.version,
     packages=['ssllabs'],
     scripts=['ssllabs-cli.py'],
     install_requires=['requests'],
     url='https://github.com/takeshixx/python-ssllabs',
-    license=vars.__license__,
+    license=vars.license,
     author='takeshix',
     author_email='takeshix@adversec.com')
